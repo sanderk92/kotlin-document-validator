31dc51fc7fd0704a11f27de84dc7f2461a7d3a65
Sander <sanderkrabbenborg@hotmail.com>
2022-07-28 00:22:51 +0200

    Simplify

--- src/main/kotlin/com/example/validations/Instantiation.kt
+++ src/main/kotlin/com/example/validations/Instantiation.kt
@@ -2,18 +2,17 @@ package com.example.validations
 
 import com.example.Document
 import com.validator.ValidationResult
-import com.validator.Validator
 import com.validator.Validator.Companion.checkEagerly
 import com.validator.Validator.Companion.checkLazily
 
-fun validateEagerly(document: Document): ValidationResult<List<String>> =
+fun validateEagerly(subject: Document): ValidationResult<List<String>> =
 
-    checkEagerly(document) {
+    checkEagerly(subject) { document ->
         // ...
     }
 
-fun validateLazily(document: Document): ValidationResult<String> =
+fun validateLazily(subject: Document): ValidationResult<String> =
 
-    checkLazily(document) {
+    checkLazily(subject) { document ->
         // ...
     }
